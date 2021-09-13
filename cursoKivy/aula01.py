e86f8e3c5790c367e81d858910ea41458471fe1c
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-09-13 20:01:45 -0300

    primeira execução

--- cursoKivy/aula01.py
+++ cursoKivy/aula01.py
@@ -1,6 +1,11 @@
 # #1 - Python Kivy - criando uma interface gráfica
 #Feito por weemerson henrique 13/09/2021
 
+#codigo ganbiarra
+import os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+#codigo ganbiarra
+
 from kivy.app import App
 from kivy.uix.button import Button
 
