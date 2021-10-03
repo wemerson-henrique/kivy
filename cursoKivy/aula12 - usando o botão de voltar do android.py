623fc8d9d4bcea7d574fb1204940cf299654347f
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-10-03 14:09:13 -0300

    fiz aula 12, as 13 e 14 é sobre gerar apk
    fiz aula 12, as 13 e 14 é sobre gerar apk e foi encidao a gerar o apk do aplicativo e instalar no celular

---
+++ cursoKivy/aula12 - usando o botão de voltar do android.py
@@ -0,0 +1,50 @@
+#Eventos da tela e Eventos de teclado
+#codigo ganbiarra
+import os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+#codigo ganbiarra
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window #importa biblioteca para criar atalhos de botão/teclado
+
+class Gerenciador(ScreenManager):
+    pass
+
+class Menu(Screen):
+    pass
+
+class Tarefas(Screen):
+    def __init__(self,tarefas=[],**kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.ids.box.add_widget(Tarefa(text=tarefa))
+    #esta class redas os Eventos de Screen, Buscar sobre
+
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.voltar)
+
+    def voltar(self,window,key,*args):
+        if key == 27:
+            App.get_running_app().root.current = 'menu'
+            return True
+            #print(key) #mostra o codigo da tecla
+
+    def on_pre_leave(self, *args):
+        Window.unbind(on_keyboard=self.voltar)
+
+    def addWidget(self):
+        texto = self.ids.texto.text
+        self.ids.box.add_widget(Tarefa(text=texto))
+        self.ids.texto.text = ""
+
+class Tarefa(BoxLayout):
+    def __init__(self,text='',**kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
+
+class Test12(App):
+    def build(self):
+        return Gerenciador()
+
+Test12().run()
