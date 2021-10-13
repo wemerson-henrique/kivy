4e8342726183a15a93a25d51b67886b049ccffd5
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2021-10-13 06:12:10 -0300

    subindo arquivos

---
+++ cursoDePythonNaPratica/aula20 - telegram.py
@@ -0,0 +1,21 @@
+import telepot
+from aula20Chatbot import Chatbot
+
+# Não criei um bot no telegram ainda, dessa forma este codigo não funciona
+# TODO: Criar bot no telegram e pegar chave
+
+telegram = telepot.Bot("Aqui vai minha chave do Telegram")
+bot = Chatbot("Hector")
+
+def recebendoMsg(msg):
+    frase = bot.escuta(frase=msg['text'])
+    resp = bot.pensa(frase)
+    bot.fala(resp)
+    #chatID = msg["chat"]["id"] # Pega o id do telegram
+    tipoMsg, tipoChat, chatID = telepot.glance(msg) # Pega o id do telegram
+    telegram.sendMessage(chatID.resp)
+
+telegram.message_loop(recebendoMsg)
+
+while True:
+    pass
