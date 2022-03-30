1229368e581c36928b66389bae9d1257b757a71b
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2022-03-30 10:05:00 -0300

    novo

--- meusExemplos/estudo/estudo1/main.py
+++ meusExemplos/estudo/estudo1/main.py
@@ -1,13 +1,28 @@
-from cProfile import run
+# codigo ganbiarra
+import os
+from turtle import onclick
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+# codigo ganbiarra
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
 
-class Home(Label):
-    pass
+class Screen1(Screen):
+    def my_callback(dt):
+        print ('#########################################################My callback is called !')
+    Clock.schedule_once(my_callback, 1)
+
+    
+
+
+class relogio():
+    def my_callback(dt):
+        print ('My callback is called !')
+    Clock.schedule_once(my_callback, 1)
 
 class MyApp(App):
-    def __init__(self):
-        return Home()
+    def build(self):
+        return Screen1()
 
-if __name__=='__main__':
-    MyApp().run()
+MyApp().run()
--- meusExemplos/layout2/main.py
+++ meusExemplos/layout2/main.py
@@ -7,19 +7,37 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class Home(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
     
-    def PostMensage(self, texto):
-        text1 = texto
-        controle = 'controle'
-        a = self.ids.input1.text
-        self.ids.input1.text = ''
-        self.ids.label1 = text1 + controle + a
-        open_text(self,a)
+    def onclick(self):
+        a = self.PegaInput()
+        self.MudarTextLabel(a)
 
-    def on_text(elf,*args):
-        self.PostMensage('oi')
+    def PegaInput(self):
+        texto = self.ids.input1.text
+        self.ids.input1.text = ''
+        return texto
+    
+    def MudarTextLabel(self,texto):
+        self.ids.label1.text = texto
+        return print(texto)
+    
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            # if the touch collides with our widget, let's grab it
+            touch.grab(self)
+            # and accept the touch.
+            return True
+    def on_touch_up(self, touch):
+        # here, you don't check if the touch collides or things likethat.
+        # you just need to check if it's a grabbed touch event
+        if touch.grab_current is self:
+            # ok, the current touch is dispatched for us.
+            # do something interesting here
+            print('Hello world!')
+            # don't forget to ungrab ourself, or you might have sideeffects
+            touch.ungrab(self)
+            # and accept the last up
+            return True
 
 class MyApp(App):
     def build(self):
